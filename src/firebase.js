eb4907b765ddf74836004b08954613a900379b89
codyrhoward <codyrhoward19@gmail.com>
2021-01-18 18:57:36 -0500

    completed nav bar and hero image

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBdBI-iqvuUH2Mdy1gl4VrNyBMhr0sDA1Q",
+    authDomain: "clone-a5bdf.firebaseapp.com",
+    projectId: "clone-a5bdf",
+    storageBucket: "clone-a5bdf.appspot.com",
+    messagingSenderId: "771308628146",
+    appId: "1:771308628146:web:3b8c33c22d7b9087ba41ab",
+    measurementId: "G-F03FSDQ36R"
+  };
